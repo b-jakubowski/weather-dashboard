145dbe958b06304db3f61730217c4a7870b622ed
Bartosz Jakubowski <bjakubov@gmail.com>
2019-11-16 21:49:37 +0100

    feat: Add Weather component to WeatherContainer

--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import City from '../components/City'
+import Weather from '../components/Weather';
 
 const WeatherContainer = () => {
-  const [error, setErrors] = useState(false)
   const [weather, setWeather] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   async function fetchData() {
     const res = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=Szczecin&APPID=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`
     )
-
+    setIsLoading(true)
     res
       .json()
-      .then(res => setWeather(res))
+      .then(res => {
+        setWeather(res)
+        setIsLoading(false)
+      }
+      )
       .catch(err => {
-        console.log(err)
-        setErrors(err)
+        console.error(err)
       })
   }
 
@@ -25,10 +29,19 @@ const WeatherContainer = () => {
 
   return (
     <>
-      {error ? (
-        <h4>Something went wrong</h4>
+      {isLoading ? (
+        <h4>Loading...</h4>
       ) : (
-        <City name={weather.name} coord={weather.coord} />
+        <>
+          <City name={weather.name} coord={weather.coord}/>
+          {weather.main &&
+            <Weather
+              temp={weather.main.temp}
+              humidity={weather.main.humidity}
+              pressure={weather.main.pressure}
+            />
+          }
+        </>
       )}
     </>
   )
