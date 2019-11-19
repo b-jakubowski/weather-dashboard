9717e2c94d99032f73391ef90f18cf606fb3b962
Bartosz Jakubowski <bjakubov@gmail.com>
2019-11-19 21:09:14 +0100

    refactor: Move Search component logic to weather container

--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -1,14 +1,17 @@
+/** @jsx jsx */
 import React, { useEffect, useState } from 'react'
+import { css, jsx } from '@emotion/core'
 import City from '../components/City'
 import Weather from '../components/Weather'
 
 const WeatherContainer = () => {
   const [weather, setWeather] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+  const [city, setCity] = useState('Szczecin')
 
-  async function fetchData() {
+  async function fetchWeather() {
     const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=Szczecin&units=metric&APPID=${process.env.REACT_APP_OPEN_WEATHER_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${process.env.REACT_APP_OPEN_WEATHER_KEY}`
     )
     setIsLoading(true)
     res
@@ -22,12 +25,46 @@ const WeatherContainer = () => {
       })
   }
 
+  function onEnterClick(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      fetchWeather()
+    }
+  }
+
   useEffect(() => {
-    fetchData()
+    fetchWeather()
   }, [])
 
   return (
     <>
+      <form>
+        <label
+          htmlFor="city"
+          css={css`
+            display: flex;
+            justify-content: center;
+            margin-bottom: 1rem;
+          `}
+        >
+          <input
+            type="text"
+            id="city-search"
+            value={city}
+            placeholder="Search for City"
+            onChange={e => setCity(e.target.value)}
+            onKeyDown={e => onEnterClick(e)}
+            css={css`
+              padding: 0.5rem;
+              margin-left: 1rem;
+              width: 12rem;
+              border-style: solid;
+              border-width: 0 0 1.5px 0;
+              border-color: grey;
+            `}
+          />
+        </label>
+      </form>
       {isLoading ? (
         <h4>Loading...</h4>
       ) : (
