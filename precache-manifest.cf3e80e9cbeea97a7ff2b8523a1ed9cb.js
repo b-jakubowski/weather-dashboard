bba2c47f2c21522ece97ecf18830d6b8bda8499b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-23 19:19:05 +0000

    Deploy b-jakubowski/weather-dashboard to github.com/b-jakubowski/weather-dashboard.git:gh-pages

---
+++ precache-manifest.cf3e80e9cbeea97a7ff2b8523a1ed9cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324067ac1a1161e9c850e4e699be3d06",
+    "url": "/weather-dashboard/index.html"
+  },
+  {
+    "revision": "bc91b1968e1543304907",
+    "url": "/weather-dashboard/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dbd004332e570091ccad",
+    "url": "/weather-dashboard/static/js/2.ce8d1c43.chunk.js"
+  },
+  {
+    "revision": "bc91b1968e1543304907",
+    "url": "/weather-dashboard/static/js/main.468e3e87.chunk.js"
+  },
+  {
+    "revision": "642cee914a7a96e25348",
+    "url": "/weather-dashboard/static/js/runtime-main.9c9a1c03.js"
+  }
+]);
